6ba6d9cfea8391412d4b53013968173f360dbf36
Nathan_syr <learncodexx>
2026-06-08 09:23:28 +0700

    [UPDATE] change new flow

--- reporter.go
+++ reporter.go
@@ -307,16 +307,6 @@ func AutoWrap(err error) error {
 	// Automatically classify common error text patterns.
 	switch {
 	case containsAny(errText,
-		"Config File",
-		"Not Found in",
-		"not found",
-		"404",
-	):
-		errType = "DATA_NOT_FOUND"
-		autoDesc = "The requested data was not found"
-		severity = SeverityDanger
-
-	case containsAny(errText,
 		"connection refused",
 		"connection reset",
 		"connection timed out",
